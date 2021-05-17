f5a77650a0cd18f4090a6eb53be13b2361e3bfe5
Reid 'arrdem' McKenzie <me@arrdem.com>
2021-05-17 13:56:35 -0600

    Tooling tweaks

--- tools/python/bzl_pytest_shim.py
+++ tools/python/bzl_pytest_shim.py
@@ -6,13 +6,5 @@ import sys
 import pytest
 
 if __name__ == "__main__":
-    print(sys.version_info, file=sys.stderr)
-    print(sys.argv, file=sys.stderr)
-
     cmdline = ["--ignore=external"] + sys.argv[1:]
-    print(cmdline, file=sys.stderr)
-
-    for e in sys.path:
-        print(f" - {os.path.realpath(e)}", file=sys.stderr)
-
     sys.exit(pytest.main(cmdline))
