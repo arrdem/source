4c5d2aaed2b21510822e4b444d04e6f7c898aec0
Reid 'arrdem' McKenzie <me@arrdem.com>
2021-05-18 19:56:29 -0600

    Pitch black overboard

--- tools/fmt.sh
+++ tools/fmt.sh
@@ -2,12 +2,11 @@
 set -euox pipefail
 cd "$(git rev-parse --show-toplevel)"
 
-bazel build //tools/python/...
+bazel build //tools/python/... //projects/reqsort
 
 DIRS=(projects tools)
 
 bazel-bin/tools/python/autoflake -ir "${DIRS[@]}"
-bazel-bin/tools/python/black "${DIRS[@]}"
 bazel-bin/tools/python/isort "${DIRS[@]}"
 bazel-bin/tools/python/unify --quote '"' -ir "${DIRS[@]}"
-bazel-bin/tools/python/reqsort --execute tools/python/requirements.txt
+bazel-bin/projects/reqsort/reqsort --execute tools/python/requirements.txt
