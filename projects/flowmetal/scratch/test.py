df3dac01c9d8840b0afb5aa8029e3d67a92331e8
Reid 'arrdem' McKenzie <me@arrdem.com>
2021-05-31 22:55:07 -0600

    Playing with the Python ASTs

---
+++ projects/flowmetal/scratch/test.py
@@ -0,0 +1,25 @@
+#!astinterp
+
+from __future__ import print_function
+
+class Foo(object):
+    def bar(self):
+        return 1
+
+    @property
+    def baz(self):
+        print("'Computing' baz...")
+        return 2
+
+
+a = Foo()
+print(a.bar())
+print(a.baz)
+
+import random
+for _ in range(10):
+    print(random.randint(0, 1024))
+
+
+def bar(a, b, **bs):
+    pass
